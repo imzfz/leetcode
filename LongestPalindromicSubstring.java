ee375b4c5dc62ab6d1a3a3ac4732c986cd5dbc42
imzfz <837553525@qq.com>
2019-02-20 15:37:33 +0800

    LongestPalindromicSubstring 马拉车算法

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,5 +1,6 @@
 /**
  * Created by zfz on 2019/2/20.
+ * 这道题好像有官方题解
  */
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
@@ -25,5 +26,28 @@ public class LongestPalindromicSubstring {
             dp[i][i] = 1;
         }
         return s.substring(start, end + 1);
+    }
+
+    //马拉车算法
+    public String longestPalindrome(String s, int unused){
+        StringBuilder t =new StringBuilder("$#");
+        for (int i = 0; i < s.length(); ++i) {
+            t.append(s.charAt(i));
+            t.append("#");
+        }
+        int p[] = new int[t.length()], id = 0, mx = 0, resId = 0, resMx = 0;
+        for (int i = 1; i < t.length(); ++i) {
+            p[i] = mx > i ? Math.min(p[2 * id - i], mx - i) : 1;
+            while (i + p[i] < t.length() && t.charAt(i + p[i]) == t.charAt(i - p[i])) ++p[i];
+            if (mx < i + p[i]) {
+                mx = i + p[i];
+                id = i;
+            }
+            if (resMx < p[i]) {
+                resMx = p[i];
+                resId = i;
+            }
+        }
+        return s.substring((resId - resMx) / 2, (resId - resMx) / 2 + resMx - 1);
     }
 }
